2f8c74b9c82e42112a89503bea0a38b46b8d3f86
Jason Waldrip <jason@waldrip.net>
2013-07-29 15:09:58 -0600

    Try to fix redirect caching

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -3,12 +3,17 @@ class ProvidersController < ApplicationController
   def show
     @provider = provider.new repo: repo
     respond_to do |format|
-      format.png { redirect_to @provider.image_url }
+      format.png { render_image }
       format.any { render }
     end
   end
 
   private
+
+  def render_image
+    expires_in 0.seconds, public: false, must_revalidate: true
+    redirect_to @provider.image_url
+  end
 
   def repo
     @repo ||= Repo.find params[:user], params[:repo], branch: branch_from_referer
