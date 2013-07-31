84df94cc3bd82d04e684cccd05f04825b3b20279
Jason Waldrip <jason@waldrip.net>
2013-07-30 22:56:00 -0600

    redirect on invalid repo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  #rescue_from Github::Error::NotFound, with: :invalid_user_or_repo
+  rescue_from Github::Error::NotFound, with: :invalid_user_or_repo
 
   def invalid_user_or_repo
     redirect_to :root, notice: 'Invalid User or Repo'
