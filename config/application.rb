6dd70a36eb558524f1d925c505a6774a77b69ea3
Jason Waldrip <jason@waldrip.net>
2013-07-31 15:17:52 -0600

    add active record back in as a requirement.

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require File.expand_path('../boot', __FILE__)
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
+require "active_record/railtie"
 require "active_model"
 # require "rails/test_unit/railtie"
 
