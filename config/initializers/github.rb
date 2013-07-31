2d13ace4332c2a7240e4b0edf3e055d6a94a1b3d
Jason Waldrip <jason@waldrip.net>
2013-07-30 22:18:58 -0600

    github now uses a token allowing for more requests per hour (5000)

--- config/initializers/github.rb
+++ config/initializers/github.rb
@@ -1,4 +1,13 @@
+require 'typhoeus/adapters/faraday'
+
 Github.configure do |config|
+  config.adapter       = :typhoeus
+  config.login         = ENV['GITHUB_USER']
+  config.password      = ENV['GITHUB_PASSWORD']
   config.client_id     = ENV['GITHUB_CLIENT_ID']
   config.client_secret = ENV['GITHUB_CLIENT_SECRET']
+end
+
+Github.configure do |config|
+  config.oauth_token = Github.oauth.create(scopes: %{public_repo}).token
 end
