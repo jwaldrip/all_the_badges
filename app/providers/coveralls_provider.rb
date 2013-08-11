066f0df8826a02159518e2508ee486774ef7ee7c
Jason Waldrip <jason@waldrip.net>
2013-08-11 14:23:09 -0600

    fix coveralls error

--- app/providers/coveralls_provider.rb
+++ app/providers/coveralls_provider.rb
@@ -1,8 +1,8 @@
 class CoverallsProvider < Provider
 
   order 4
-  link_url 'https://coveralls.io/r/:user/:repo_name?branch=:branch'
-  image_url 'https://coveralls.io/repos/:user/:repo_name/badge.png?branch=:branch'
+  link_url 'https://coveralls.io/r/:user_login/:repo_name?branch=:branch'
+  image_url 'https://coveralls.io/repos/:user_login/:repo_name/badge.png?branch=:branch'
   creatable! link_url: 'https://coveralls.io/', image_url: 'coveralls_unknown.png'
 
 end
