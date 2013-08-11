3b3fb08fe064d7254b61159aeb89904e4538f7fd
Jason Waldrip <jason@waldrip.net>
2013-08-10 23:09:14 -0600

    simplify content class

--- app/models/content.rb
+++ app/models/content.rb
@@ -3,24 +3,24 @@ class Content
   include SelectiveAttributes
 
   class << self
-
+    include Cacheable
+    cache_keys :name
+    
     def find(repo, path)
-      response = Rails.cache.fetch repo.cache_key(path), expires_in: 60.minutes do
-        find_without_cache(repo, path)
-      end
+      response = Github.repos.contents.find(repo.user_login.to_s, repo.name.to_s, path, ref: repo.branch.to_s).body
       case response
       when Array
-        new_collection_from_response(response, repo)
+        new_collection_from_response response, repo
       when Hash
-        new response
+        new_instance_from_response response, repo
       end
     rescue Github::Error::NotFound
       []
     end
 
-    def find_without_cache(repo, path)
-      Github.repos.contents.find(repo.user_login.to_s, repo.name.to_s, path, ref: repo.branch.to_s).body
-    end
+    cache_method :find, expires_in: 60.minutes
+
+    private
 
     def new_collection_from_response(response, repo)
       response.map { |file| new_instance_from_response file, repo }
@@ -38,6 +38,8 @@ class Content
   def read
     reload unless @content
     Base64.decode64 @content
+  rescue NoMethodError
+    nil
   end
 
   def reload
